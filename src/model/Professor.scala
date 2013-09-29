81e98f454c06242a7c17ff4935c3a4c11dd9299a
JonaKoelbl <jonakoelbl@gmail.com>
2013-09-29 12:31:21 -0300

    Update Professor.scala

--- src/model/Professor.scala
+++ src/model/Professor.scala
@@ -1,11 +1,21 @@
 package model
 
-case class Professor (aName: String, aLastname: String, anAge: Int)
-	extends Person (aName, aLastname, anAge) with Schedule with AcademicQuery{
+case class Professor(aName: String, aLastname: String, anAge: Int)
+  extends Person(aName, aLastname, anAge) with Schedule with AcademicQuery {
 
-  def getAcademicRecord ={
-    val allCourses = AcademicData.Students.foldRight(List[(Course, Boolean, Int)]())((a, b) => a.courses ++ b)
-    allCourses.filter(p => p._1.professors.equals(this))//
-    		  .foldRight(List[(Boolean, Int)]())((a, b) => (a._2, a._3) :: b) 
+  private def getAllCourses: List[(Course, Boolean, Int)] = {
+    AcademicData.Students.foldRight(List[(Course, Boolean, Int)]())((a, b) => a.noteTablesAllCourses ::: b)
+  }
+
+  def getAcademicRecord = {
+    this.getAllCourses.filter(p => p._1.professors.contains(this)) //
+      .foldRight(List[(Boolean, Int)]())((a, b) => (a._2, a._3) :: b)
+  }
+
+  def annualAverage(aYear: Int): Professor = {
+    new Professor(aName, aLastname, anAge) with AcademicQuery {
+      val notesWithXYear = this.getAllCourses.filter(n => n._1.aDate.getYear().equals(aYear))
+      override def getAcademicRecord = notesWithXYear.foldRight(List[(Boolean, Int)]())((a, b) => (a._2, a._3) :: b)
+    }
   }
 }
