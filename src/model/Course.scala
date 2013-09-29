244705141ec16d6b0a981c48d81d5765d6e4a7f4
JonaKoelbl <jonakoelbl@gmail.com>
2013-09-29 12:30:23 -0300

    Update Course.scala

--- src/model/Course.scala
+++ src/model/Course.scala
@@ -2,15 +2,16 @@ package model
 
 import java.util.Date
 
-class Course (val subject: Subject,val professors: List[Professor],val students: List[Student], implicit val aDate : Date = new Date)
-		extends WithClassRoom with AcademicQuery{
-  
-  def neededSpace : Int = {
-    students.length + professors.length 
+class Course(val subject: Subject, val professors: List[Professor], val students: List[Student], implicit val aDate: Date = new Date)
+  extends WithClassRoom with AcademicQuery {
+
+  def neededSpace: Int = {
+    students.length + professors.length
   }
 
-  def getAcademicRecord ={
-    val allNotes = students.foldRight(List[(Course, Boolean, Int)]())(_.courses ++ _).filter(c=> c._1.subject.aName.equals(this.subject.aName))
-    allNotes.foldRight(List[(Boolean, Int)]())((a, b) => (a._2, a._3)::b)
+  def getAcademicRecord: List[(Boolean, Int)] = {
+    val allCourses: List[(Course, Boolean, Int)] = students.foldRight(List[(Course, Boolean, Int)]())(_.noteTablesAllCourses ::: _)
+    val allNotes = allCourses.filter(c => c._1.subject.aName.equals(this.subject.aName) && c._1.equals(this))
+    allNotes.foldRight(List[(Boolean, Int)]())((a, b) => (a._2, a._3) :: b)
   }
 }
