32eefe7556c936075a4b498a979bf84a0e8deb38
JonaKoelbl <jonakoelbl@gmail.com>
2013-09-29 12:29:03 -0300

    Update Career.scala

--- src/model/Career.scala
+++ src/model/Career.scala
@@ -3,10 +3,10 @@ package model
 class Career (val aName: String,var subjects: List[Subject]) extends AcademicQuery{
 	
 	def getAcademicRecord = {
-	  val allCourses = subjects.foldRight(List[Course]())((a, b)=> a.courses ++ b)
-	  val allStudent = allCourses.foldRight(List[Student]())((a,b)=> a.students ++ b)
+	  val allCourses = subjects.foldRight(List[Course]())((a, b)=> a.courses ::: b)
+	  val allStudent = allCourses.foldRight(List[Student]())((a,b)=> a.students ::: b)
 	  
-	  allStudent.foldRight(List[(Course, Boolean, Int)]())((a,b)=> a.courses ++ b)//
+	  allStudent.foldRight(List[(Course, Boolean, Int)]())((a,b)=> a.noteTablesAllCourses ::: b)//
 	  			.foldRight(List[(Boolean, Int)]())((a,b)=> (a._2, a._3) ::b)
 	}
 }
